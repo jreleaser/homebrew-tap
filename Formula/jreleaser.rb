5eae5ead22c19cea97afff653820275c9cc33ea2
jreleaserbot <jreleaser@kordamp.org>
2021-05-08 08:34:58 +0000

    jreleaser v0.3.0

--- Formula/jreleaser.rb
+++ Formula/jreleaser.rb
@@ -1,9 +1,9 @@
 class Jreleaser < Formula
   desc "Release Java projects quickly and easily with JReleaser"
   homepage "https://jreleaser.org"
-  version "0.2.0"
-  url "https://github.com/jreleaser/jreleaser/releases/download/v0.2.0/jreleaser-0.2.0.zip"
-  sha256 "a70268d8047bd8cb05226a8d5ca840aa5ca2b1f19741908e0c76fee2b46f45f0"
+  version "0.3.0"
+  url "https://github.com/jreleaser/jreleaser/releases/download/v0.3.0/jreleaser-0.3.0.zip"
+  sha256 "c1af5faa28e6a454031f739664833d89cf6743760e419a4d1f0e42789b954e73"
   license "Apache-2.0"
 
   bottle :unneeded
@@ -17,6 +17,6 @@ class Jreleaser < Formula
 
   test do
     output = shell_output("#{bin}/jreleaser --version")
-    assert_match "0.2.0", output
+    assert_match "0.3.0", output
   end
 end
