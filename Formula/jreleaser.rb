69ec0403c8c25dd4fcf887dc510e204ea821ab32
jreleaserbot <jreleaser@kordamp.org>
2021-11-27 18:02:13 +0000

    jreleaser v0.9.0

--- Formula/jreleaser.rb
+++ Formula/jreleaser.rb
@@ -3,7 +3,7 @@ class Jreleaser < Formula
   homepage "https://jreleaser.org"
   url "https://github.com/jreleaser/jreleaser/releases/download/v0.9.0/jreleaser-0.9.0.zip"
   version "0.9.0"
-  sha256 "91d4f7df46122d83dd77029f290f69191d6305a9142c059be458f8cd1f4f7838"
+  sha256 "2a0e82f59400bfa0e87ed42ca10a824ce830d856df28a944e1ae1467cdad18fa"
   license "Apache-2.0"
 
   depends_on "openjdk@8"
